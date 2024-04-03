eccfd50e975c4e58db0d779eaaa9207faef73a5c
Paul Mason <paul@buyerresist.ca>
2024-04-03 14:53:21 -0700

    fix failing test

--- src/components/ResultsDisplay/ResultsDisplay.test.tsx
+++ src/components/ResultsDisplay/ResultsDisplay.test.tsx
@@ -1,9 +1,14 @@
 import { render, screen } from "@testing-library/react";
 import { ResultsDisplay } from "./ResultsDisplay";
+import { TvShowProvider } from "../TvShowContext/TvShowContext";
 
 describe("ResultsDisplay", () => {
   beforeEach(() => {
-    render(<ResultsDisplay />);
+    render(
+      <TvShowProvider>
+        <ResultsDisplay />
+      </TvShowProvider>
+    );
   });
   it("renders the results display", () => {
     const resultsDisplay = screen.getByText(/results display/i);
